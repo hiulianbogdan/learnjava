12feab29f63e8bc4b3b4ad3c2ad76064bd878420
hiulianbogdan <47738202+hiulianbogdan@users.noreply.github.com>
2026-03-29 19:26:11 +0300

    commit messeage

--- src/temajava/animal.java
+++ src/temajava/animal.java
@@ -13,6 +13,8 @@ public class animal {
 	public void displayName() {
 		System.out.printf("Your animal name is %s\n", getName());
 	}
-	
+	public void displayName() {
+		System.out.printf("Your animal name is %s\n", getName());
+	}
 	
 }
